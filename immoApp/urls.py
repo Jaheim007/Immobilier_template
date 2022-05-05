fad718c2ddf2911a225fd9c3935e1d80073d30ed
iMac-13 <imac-13@192.168.50.10>
2022-05-05 16:28:10 +0000

    complete website structure done

--- immoApp/urls.py
+++ immoApp/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about',views.about, name='about' ),
-    path('blogGrid', views.blogGrid , name='blogGrid'),
+    path('about',views.about, name='about'),
+    path('blog', views.blogGrid , name='blog'),
     path('blogSingle', views.blogSingle , name='blogSingle'),
     path('agentSingle', views.agentSingle , name='agentSingle'),
     path('agentGrid', views.agentGrid , name='agentGrid'),
-    path('Contact', views.contact , name='contact'),
-    path('propertyGrid', views.propertyGrid , name='propertyGrid'),
+    path('contact', views.contact , name='contact'),
+    path('property', views.propertyGrid , name='property'),
     path('propertySingle', views.propertySingle , name='propertySingle'),
 ]
