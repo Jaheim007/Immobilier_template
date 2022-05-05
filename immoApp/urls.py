da365ad48c8b7fcc1b6a77820122d36d40736807
iMac-13 <imac-13@192.168.50.10>
2022-05-05 13:41:12 +0000

    created the website

--- immoApp/urls.py
+++ immoApp/urls.py
@@ -1,5 +1,15 @@
+from unicodedata import name
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about',views.about, name='about' ),
+    path('blogGrid', views.blogGrid , name='blogGrid'),
+    path('blogSingle', views.blogSingle , name='blogSingle'),
+    path('agentSingle', views.agentSingle , name='agentSingle'),
+    path('agentGrid', views.agentGrid , name='agentGrid'),
+    path('Contact', views.contact , name='contact'),
+    path('propertyGrid', views.propertyGrid , name='propertyGrid'),
+    path('propertySingle', views.propertySingle , name='propertySingle'),
 ]
--- immoApp/views.py
+++ immoApp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+def home(request): 
+    return render(request, 'EstateAgency/EstateAgency/index.html')
+
+def about(request):   
+    return render(request,'EstateAgency/EstateAgency/about.html' )
+
+def contact(request):     
+    return render(request,'EstateAgency/EstateAgency/contact.html' )
+
+def agentSingle(request):         
+    return render(request,'EstateAgency/EstateAgency/agent-single.html' )
+
+def agentGrid(request):       
+    return render(request,'EstateAgency/EstateAgency/agents-grid.html' ) 
+
+def propertySingle(request):        
+    return render(request,'EstateAgency/EstateAgency/property-single.html' )
+
+def propertyGrid(request):          
+    return render(request,'EstateAgency/EstateAgency/property-grid.html' )
+
+def blogSingle(request):     
+    return render(request,'EstateAgency/EstateAgency/blog-single.html' )
+
+def blogGrid(request):     
+    return render(request,'EstateAgency/EstateAgency/blog-grid.html')
+
+
 # Create your views here.
