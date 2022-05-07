8a390ce9d3ebcff563a75bd9d6324277267f3df7
Jaheim007 <jaheimkouaho@gmail.com>
2022-05-07 09:54:26 +0000

    immobiler

--- immoApp/admin.py
+++ immoApp/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
-from .models import Contact , Customers ,Silder , SiteInfo
+from .models import Contact , Customers ,Silder , SiteInfo, House, Agent
+from django.utils.safestring import mark_safe
 
-admin.site.register(Contact)
+
+class Contactsviews(admin.ModelAdmin):
+    list_display = ('faceLink', 'InstaLink', 'twitterLink','SiteContact', 'WaltLink', 'linkedLink', 'mainPhone', 'email', 'lat','lomge')
+admin.site.register(Contact, Contactsviews)
+
+class SiteViews(admin.ModelAdmin):
+    list_display = ('title', 'mainColor', 'fullSiteColor')
+admin.site.register(SiteInfo, SiteViews)
+
+
+class Sliderviews(admin.ModelAdmin):
+    list_display = ('title1', 'title2', 'title3','Img1', 'Img2', 'Img3')
+    
+    def views(self , obj):    
+        return mark_safe(f'<img src="{obj.image.url}" style="height:100px; width:200px">')  
+
+admin.site.register(Silder, Sliderviews)
+
+class HouseViews(admin.ModelAdmin):
+    list_display = ('rooms', 'garage', 'toillets', 'addressName', 'lat', 'long')
+
+admin.site.register(House, HouseViews)
+
+class Customerviews(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'email', 'birthday', 'phone', 'image', 'username', 'password')
+    
+    def views(self , obj):    
+        return mark_safe(f'<img src="{obj.image.url}" style="height:100px; width:200px">')  
+
+admin.site.register(Customers, Customerviews)
+
+class AgentViews(admin.ModelAdmin):
+    list_display = ('biography', 'image', 'fb_link', 'insta_link', 'twitter_link')
+
+admin.site.register(Agent, AgentViews)
 
 # Register your models here.
--- immoApp/models.py
+++ immoApp/models.py
@@ -1,15 +1,21 @@
-from pyexpat import model
 from django.db import models
-
-#Fronts table
 
 class SiteInfo(models.Model):
     title = models.CharField(max_length=200)
     mainColor = models.CharField(max_length=200)
     fullSiteColor = models.CharField(max_length=200)
 
+    
+    class Meta():
+        verbose_name = 'Site Info'
+        verbose_name_plural = 'Site Infos'
+    
+    def __str__(self):
+        return self.title
+
+
 class Contact(models.Model):
-    faceLink = models.CharField(max_length=200)    
+    faceLink = models.CharField(max_length=100) 
     InstaLink = models.CharField(max_length=200)    
     twitterLink = models.CharField(max_length=200)    
     SiteContact= models.CharField(max_length=200)    
@@ -19,28 +25,83 @@ class Contact(models.Model):
     email = models.EmailField(max_length=200)    
     lat = models.CharField(max_length=200)    
     lomge = models.CharField(max_length=200)  
+    
+    class Meta():
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+    
+    def __str__(self):
+        return self.faceLink
+
+    
 
 class Silder(models.Model):
-    Img1 = models.ImageField()
-    Img2 = models.ImageField()
-    Img3 = models.ImageField()
-    title1 = models.CharField(max_length=200)
+    title1 = models.CharField(max_length=100)
     title2 = models.CharField(max_length=200)
     title3 = models.CharField(max_length=200)
+    Img1 = models.FileField()
+    Img2 = models.FileField()
+    Img3 = models.FileField()
 
-#App User
+    class Meta():
+        verbose_name = 'Silder Image & Title'
+        verbose_name_plural = 'Silder Images & Titles'
+    
+    
+    def __str__(self):
+        return self.title1
+
+
 
 class Customers(models.Model):
     nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    birthday = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    image = models.FileField()
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    class Meta():
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
 
     def __str__(self):
         return self.nom
 
+    
 
-# Create your models here.
+class Agent(models.Model):
+    biography = models.CharField(max_length=500)
+    image = models.FileField()
+    fb_link =  models.CharField(max_length=200) 
+    insta_link = models.CharField(max_length=200) 
+    twitter_link = models.CharField(max_length=200)
+    
+    class Meta():
+        verbose_name = 'Agent'
+        verbose_name_plural = 'Agents'
+    
+    def __str__(self):
+        return self.biography 
+    
+
+
+class House(models.Model):
+    rooms = models.CharField(max_length=100)
+    garage = models.CharField(max_length=100)
+    toillets = models.CharField(max_length=100)
+    addressName = models.CharField(max_length=100)
+    lat = models.CharField(max_length=100)
+    long = models.CharField(max_length=100)
+
+
+    class Meta():
+        verbose_name = 'Maison'
+        verbose_name_plural = 'Maisons'
+
+    def __str__(self):
+        return self.rooms
+
+
